f0f3198ce7a0485cd9202af4866e62be962e4916
colyk <colykxer@gmail.com>
2020-02-10 20:45:34 +0100

    wip

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import os.path as op
 import random
 
 import telegram
@@ -14,13 +15,20 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+BAD_FILE_ERROR = "Bad file."
+MAX_SENTENCE_COUNT = 100
+MIN_SENTENCE_COUNT = 1
+
 
 def send_typing_action(func):
     def wrapper(self, update, context):
-        context.bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+        context.bot.send_chat_action(
+            chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING
+        )
         func(self, update, context)
 
     return wrapper
+
 
 class Bot:
     def __init__(self):
@@ -29,13 +37,14 @@ class Bot:
             logger.error('Set "telegram_token" environment variable.')
             logger.error("export telegram_token=example_token")
             exit(os.EX_CONFIG)
-        
-        self.last_command = ''
+
+        self.last_command = ""
+        self.sentence_count = 5
         self.updater = Updater(self.token, use_context=True)
         self.set_commands_handlers()
 
-        self.static_dir = os.path.join('.', 'static')
-        if not os.path.isdir(self.static_dir):
+        self.static_dir = op.join(".", "static")
+        if not op.isdir(self.static_dir):
             os.mkdir(self.static_dir)
 
     def local_run(self):
@@ -63,6 +72,7 @@ class Bot:
         dp.add_handler(CommandHandler("load", self.on_load))
         dp.add_handler(CommandHandler("list", self.on_list))
         dp.add_handler(CommandHandler("markov", self.on_markov))
+        dp.add_handler(CommandHandler("setSentenceCount", self.on_set_sentence_count))
 
         dp.add_handler(MessageHandler(Filters.text, self.on_unknown))
 
@@ -71,7 +81,7 @@ class Bot:
 
     def on_start(self, update, context):
         self.on_help(update, context)
-        self.last_command = ''
+        self.last_command = ""
 
     def on_help(self, update, context):
         help_text = (
@@ -81,67 +91,105 @@ class Bot:
             "/setSentenceCount - set sentence count\n"
         )
         update.message.reply_text(help_text)
-        self.last_command = ''
+        self.last_command = ""
 
     @send_typing_action
     def on_unknown(self, update, context):
-        if self.last_command == 'markov':
-            r = self.calc_markov(os.path.join(self.static_dir, update.message.text))
+        msg = update.message.text
+
+        if self.last_command == "markov":
+            r = self.calc_markov(op.join(self.user_dir(update), msg))
             update.message.reply_text(r)
+        elif self.last_command == "sentence_count":
+            try:
+                sentence_count = int(msg)
+            except Exception as e:
+                update.message.reply_text("Bad value.")
+            else:
+                if MIN_SENTENCE_COUNT < int(msg) < MAX_SENTENCE_COUNT:
+                    self.sentence_count = int(msg)
+                update.message.reply_text(f"New sentence count: {self.sentence_count}")
         else:
-            update.message.reply_text(f'on unknown command "{update.message.text}"')
-        
-        self.last_command = ''
+            update.message.reply_text(f'on unknown command "{msg}"')
+        self.last_command = ""
 
     def on_error(self, update, context):
         logger.warning('Update "%s" caused error "%s"', update, context.error)
-        self.last_command = ''
+        self.last_command = ""
 
     def on_load(self, update, context):
-        update.message.reply_text(f'Load your file.')
-        self.last_command = ''
+        update.message.reply_text(f"Load your file.")
+        self.last_command = ""
 
     def on_file(self, update, context):
         file_id = update.message.document.file_id
         file_name = update.message.document.file_name
         file = context.bot.getFile(file_id)
-        file.download(os.path.join(self.static_dir, file_name))
-        update.message.reply_text(f'File successfully downloaded.')
-        self.last_command = ''
+        file.download(op.join(self.user_dir(update), file_name))
+        update.message.reply_text(f"File successfully downloaded.")
+        self.last_command = ""
 
     def on_list(self, update, context):
-        files = ', '.join(os.listdir(self.static_dir))
+        files = ", ".join(os.listdir(self.user_dir(update)))
         update.message.reply_text(files)
-        self.last_command = ''
+        self.last_command = ""
 
     def on_markov(self, update, context):
-        self.last_command = 'markov'
+        self.last_command = "markov"
 
-        menu_keyboard = [os.listdir(self.static_dir)]
-        menu_markup = telegram.ReplyKeyboardMarkup(menu_keyboard, one_time_keyboard=True, resize_keyboard=True)
-        self.updater.bot.sendMessage(chat_id=update.message.chat_id, text="Choose file:", reply_markup=menu_markup)
-        
+        menu_keyboard = [os.listdir(self.user_dir(update))]
+        menu_markup = telegram.ReplyKeyboardMarkup(
+            menu_keyboard, one_time_keyboard=True, resize_keyboard=True
+        )
+        self.updater.bot.sendMessage(
+            chat_id=update.message.chat_id,
+            text="Choose file:",
+            reply_markup=menu_markup,
+        )
+
     def calc_markov(self, file):
-        with open(file, encoding="utf-8") as f:
-            propabilities = get_words_propabilities(f.read())
+        try:
+            with open(file, encoding="utf-8") as f:
+                propabilities = get_words_propabilities(f.read())
+        except FileNotFoundError:
+            return BAD_FILE_ERROR
 
-        m = MarkovChain(MarkovState(random.choice(list(propabilities.keys()))), history=200)
+        init_word = random.choice(list(propabilities.keys()))
+        mc = MarkovChain(MarkovState(init_word), history=200)
         for word, prop in propabilities.items():
             state_1 = MarkovState(word)
             for word_2, value in prop.items():
                 state_2 = MarkovState(word_2)
-                m.add_probability(state_1, state_2, value)
+                mc.add_probability(state_1, state_2, value)
+
+        words_treshold = self.sentence_count * 25
         result = "."
-        for word in m:
-            if result.count(".") > 5:
+        for word in mc:
+            if result.count(".") > self.sentence_count:
                 break
+            if result.count(" ") > words_treshold:
+                return BAD_FILE_ERROR
             if result.endswith("."):
                 word = word.capitalize()
             result += f" {word}"
 
-        print(result)
-        return result[1:]
+        return f"Started from word: {init_word}\n{result[1:]}"
+
+    def user_dir(self, update):
+        user_dir_path = op.join(self.static_dir, str(update.message.chat_id))
+        if not op.isdir(user_dir_path):
+            os.mkdir(user_dir_path)
+            example_file = op.join(".", "example_text.txt")
+            os.link(example_file, op.join(user_dir_path, "example_OSHO"))
+        return user_dir_path
+
+    def on_set_sentence_count(self, update, context):
+        self.last_command = "sentence_count"
+
+        response = f"Old sentence count: {self.sentence_count}\nProvide new value"
+        update.message.reply_text(response)
+
 
 if __name__ == "__main__":
     bot = Bot()
-    bot.web_run()
+    bot.local_run()
--- text_propabilities.py
+++ text_propabilities.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-from time import time
 from markov_chain import MarkovChain, MarkovState
 import random
 
@@ -21,29 +19,3 @@ def get_words_propabilities(text: str):
         for k, e in choices.items():
             propabilities[key][k] = e / events
     return propabilities
-
-
-if __name__ == "__main__":
-    with open("example_text.txt", encoding="utf-8") as f:
-        s = time()
-        propabilities = get_words_propabilities(f.read())
-        print(time() - s)
-    s = time()
-
-    m = MarkovChain(MarkovState(random.choice(list(propabilities.keys()))), history=500)
-    for word, prop in propabilities.items():
-        state_1 = MarkovState(word)
-        for word_2, value in prop.items():
-            state_2 = MarkovState(word_2)
-            m.add_probability(state_1, state_2, value)
-    print(time() - s)
-
-    result = "."
-    for word in m:
-        if result.count(".") > 5:
-            break
-        if result.endswith("."):
-            word = word.capitalize()
-        result += f" {word}"
-
-    print(result[1:])
